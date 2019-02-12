d90ee4c3c3275ad70016277db77931780110d989
Charles Hamilton <crhamiltonj@gmail.com>
2019-02-11 22:31:06 -0500

    Created logic/css for SeasonDisplay and added spinner.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
+            (position) => this.setState({lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message }))
     }
-
 
     render() {
         if (this.state.errorMessage && !this.state.lat ) {
             return <div>Error: { this.state.errorMessage }</div>
         }
         if (!this.state.errorMessage && this.state.lat ) {
-            return <div>Latitude: { this.state.lat }</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Loading...</div>
+        return <Spinner message="Plase accept location request..."/>
+        // return <Spinner />
+
     }
 }
 
